2e37cfbe68ba501ab7764c3fbb37119179ef54ca
Sarif Tachamo <112578172+sariftachamo-9@users.noreply.github.com>
2026-08-24 22:15:32 +0545

    Delete backend directory

--- backend/src/routes/history.routes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getHistory } = require('../controllers/history.controller');
-
-// GET /api/history - Retrieve list of successfully processed media files
-router.get('/', getHistory);
-
-module.exports = router;
